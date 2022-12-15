fb8306e7e0597bfbb6587ce62828fc0cb28ea1a0
Артём <chera1904@mail.ru>
2022-12-15 12:41:41 +0300

    added typing for Session class

--- session.py
+++ session.py
@@ -1,5 +1,8 @@
+from typing import Dict
+
+
 class Session:
-    list_session = {}
+    list_session: Dict = {}
 
     def add_session(self, chat_id, data=""):
         self.list_session[chat_id] = data
